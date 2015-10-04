a7ba2309c2e9fe2a68d4cbb570fbd0f49253c047
lmingcsce <Ming.Aaron.Liu@gmail.com>
2015-10-04 11:43:25 -0700

    sync

--- UnixPipe/pipe_shell.c
+++ UnixPipe/pipe_shell.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <ctype.h>
 #include <readline/readline.h>
 #include <readline/history.h>
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
 	const char *prompt = "mgliu>>";
 	const char *delim = "|";
 	char *input_cmd, *token, *str;
-	int i;
+	int i, invalid;
 
 	while (input_cmd = readline(prompt)) {
 #ifdef DEBUG
@@ -24,22 +24,79 @@ int main(int argc, char **argv)
 		}
 
 
-		for (i = 1, str = input_cmd; ; i++, str= NULL) {
+		for (invalid = 0, i = 1, str = input_cmd; ; i++, str= NULL) {
 			token = strtok(str, delim);
 			if (token == NULL)
 				break;
 			if (!cmd_valid(token)) {
 				printf("syntax error near unexpected token \'|\'\n");
+				invalid = 1;
 				break;
 			}
+			add_cmdlist(token);
 #ifdef DEBUG
 			printf("\t%d: %s\n", i, token);
 #endif
 		}
 
+#ifdef DEBUG
+		if (!invalid) {
+			print_cmdlist();
+		}
+#endif
+		if (!invalid) {
+			cmd_run();
+		}
+
+		free_cmdlist();
 	}
 
 	return 0;
+}
+
+void cmd_run()
+{
+}
+
+void add_cmdlist(char *cmd) 
+{
+	node *tmp, *tmp1;
+
+	tmp = (node *) malloc(sizeof(node));
+	memset(tmp, 0x00, sizeof(node));
+
+	tmp->cmd = cmd;
+	tmp->next = NULL;
+
+	if (cmdhead == NULL) {
+		cmdhead = tmp;
+	} else {
+		tmp1 = cmdhead;
+		while(tmp1->next != NULL)
+			tmp1 = tmp1->next;
+		tmp1->next = tmp;
+	}
+}
+
+void free_cmdlist()
+{
+	node *tmp = cmdhead;
+
+	while (cmdhead != NULL) {
+		tmp = tmp->next;
+		free(cmdhead);
+		cmdhead =  tmp;
+	}
+}
+
+void print_cmdlist()
+{
+	node *tmp = cmdhead;
+
+	while (tmp != NULL) {
+		printf("LIST --> %s\n", tmp->cmd);
+		tmp = tmp->next;
+	}
 }
 
 int input_valid(char *cmd)
--- UnixPipe/pipe_shell.h
+++ UnixPipe/pipe_shell.h
@@ -1,8 +1,20 @@
 #ifndef _PIPE_SHELL_H
 #define _PIPE_SHELL_H
 
+#include <stdio.h>
+
 #define DEBUG
 int cmd_valid(char *cmd);
 int input_valid(char *cmd);
+
+typedef struct _node{
+	char *cmd;
+	struct _node *next;
+} node;
+
+node *cmdhead = NULL;
+void add_cmdlist(char *cmd);
+void free_cmdlist();
+void print_cmdlist();
 
 #endif /* _PIPE_SHELL_H */
